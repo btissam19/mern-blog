e3d58d746d5cdc82f93d60b861796f48519ce0b8
Ibtissam <btissam1234btissam12@gmail.com>
2024-02-10 15:56:35 +0100

    changes for deployment

--- api/index.js
+++ api/index.js
@@ -7,10 +7,12 @@ import error from "./middlewares/error.js"
 import cookieParser from 'cookie-parser';
 import postRoutes from './routes/post.route.js';
 import commentRoutes from './routes/comment.route.js';
+import path from 'path';
 
 mongoose.connect(process.env.MONGO)
 .then(()=>console.log("connected"))
 .catch((err)=>console.log(err))
+const __dirname = path.resolve();
 
 const app = express();
 app.use(express.json());
@@ -20,6 +22,11 @@ app.use(cookieParser());
 app.use('/api/user',userRoute);
 app.use('/api/post', postRoutes);
 app.use('/api/comment', commentRoutes);
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 app.use(error);
 
 app.listen(3000,()=>console.log("app listen in port 3000"))
